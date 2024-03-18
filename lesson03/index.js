1f489826a7465d988289c112bed075ddb336f5c8
Bardia Khademi <138980378+bardlur@users.noreply.github.com>
2024-03-18 04:21:18 -0700

    Update index.js

--- lesson03/index.js
+++ lesson03/index.js
@@ -7,16 +7,18 @@ let num6 = -5;
 let num7 = 0;
 let notANumber = "true";
 let result = num1 / notANumber
+
 console.log(num1 + num2);
 console.log(num1 * num2);
 console.log(num1 / num2);
 console.log(num1 - num2);
+
 console.log(result, typeof result);
 console.log(num7++) // num7 = num7 + 1
 console.log(++num7) // num7 = num7 + 1
 console.log(num7--) // num7 = num7 - 1
 console.log(--num7) // num7 = num7 - 1
-console.log(2 ** 3);
+console.log(2 ** 3); // => 8
 console.log(num7 ** num1);
-console.log(typeof 1234567890123456789012345678901234567890n);
-console.log(4%3)
+console.log(typeof 1234567890123456789012345678901234567890n);   // Big number
+console.log(4%3) // => 1
